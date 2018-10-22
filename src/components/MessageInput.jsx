f7dea080889c756a7a907be3690ee1f20253e89d
Daniel Yoo <yoo.d93@gmail.com>
2018-10-22 19:41:46 -0400

    have function create message input

--- src/components/MessageInput.jsx
+++ src/components/MessageInput.jsx
@@ -1,10 +1,34 @@
 import React, {Component} from 'react';
+import {addMessage} from '../firebase/messages';
 
 class MessageInput extends Component {
+    state = {
+        value: ''
+    }
+
+    constructor(props) {
+        super(props);
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(e) {
+        e.preventDefault();
+        this.setState({value: e.target.value});
+    }
+
+    handleSubmit(e) {
+        e.preventDefault();
+
+        addMessage(this.state.value, 'Daniel Yoo');
+
+        this.setState({value: ''});
+    }
+
     render() {
         return (
-            <form>
-                <input type='text' placeholder='Press Enter to send'/>
+            <form onSubmit={this.handleSubmit}>
+                <input type='text' value={this.state.value} onChange={this.handleChange} placeholder='Press Enter to send'/>
             </form>
         )
     }
