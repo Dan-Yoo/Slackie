aa629ca6c25bd99dd439e877d222b21e245f305e
Daniel Yoo <yoo.d93@gmail.com>
2018-10-23 02:12:48 -0400

    implemented login and logout with rendering

--- src/App.js
+++ src/App.js
@@ -1,19 +1,37 @@
 import React, { Component } from 'react';
 import {BrowserRouter as Router, Route} from 'react-router-dom';
+import { authState } from 'rxfire/auth';
+import {auth} from './firebase';
 import NavBar from './components/NavBar';
 import Chat from './pages/Chat';
 import Login from './pages/Login';
 import './App.css';
 
 class App extends Component {
+  state = {
+    loggedIn: false
+  }
+
+  constructor() {
+    super()
+  }
+
+  componentDidMount() {
+    authState(auth).subscribe(user => {
+      if (user) {
+        this.setState({loggedIn: true});
+      } else {
+        this.setState({loggedIn: false});
+      }
+    });
+  }
 
   render() {
     return (
       <Router>
         <main>
-          <NavBar />
-          <Route exact path="/" component={Login} />
-          <Route path="/chat" component={Chat} />
+          <NavBar loggedIn={this.state.loggedIn} />
+          {(this.state.loggedIn) ? <Chat /> : <Login />}
         </main>
       </Router>
     )
